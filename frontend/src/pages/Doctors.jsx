8ca83dcecca804dd31cf6ebb90502e4c749255ad
mayank-bhardwaj12 <bhardwajmayank058@gmail.com>
2026-02-19 23:33:48 +0530

    Navbar updated

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -4,7 +4,8 @@ import { AppContext } from '../context/AppContext'
 
 const Doctors = () => {
   const { speciality } =useParams ()
-  const[filterDoc,setFilterDoc]=useState([]);
+  const[filterDoc,setFilterDoc]=useState([])
+  const[showFilter,setShowFilter]=useState(false)
   const navigate=useNavigate()
   const {doctors}=useContext(AppContext)
   const applyFilter=()=>{
@@ -22,7 +23,8 @@ const Doctors = () => {
     <div>
       <p className='text-gray-600'>Select the specialist</p>
       <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
-        <div className=' flex flex-col gap-4 text-sm text-gray-600    '>
+      <button className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showFilter?'bg-primary text-white':''}`} onClick={()=>setShowFilter(prev=>!prev)}>Filters</button>
+        <div className={`flex-col gap-4 text-sm text-gray-600 ${showFilter?'flex':'hidden sm:flex'}`}>
             <p onClick={()=>speciality === 'General physician' ? navigate('/doctors') : navigate('/doctors/General physician')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "General physician" ? " bg-green-100 text-black" : ""}`}>General physician</p>
             <p onClick={()=>speciality === 'Gynecologist' ? navigate('/doctors') : navigate('/doctors/Gynecologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Gynecologist" ? " bg-green-100 text-black" : ""}`}>Gynecologist</p>
             <p onClick={()=>speciality === 'Dermatologist' ? navigate('/doctors') : navigate('/doctors/Dermatologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Dermatologist" ? " bg-green-100 text-black" : ""}`}>Dermatologist</p>
